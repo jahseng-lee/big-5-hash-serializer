6399ec53f7edbfccc228a7ccdae9dfdbaad3427e
Jah Seng Lee <jahseng.lee@gmail.com>
2019-03-15 22:55:38 +1300

    Implement happy path for report parsing
    Given a fully sanitary report will parse and return expected hash

--- big_five_results_text_serializer.rb
+++ big_five_results_text_serializer.rb
@@ -1,10 +1,82 @@
 class BigFiveResultsTextSerializer
-  attr_reader :text
+  class InvalidReportError < StandardError; end
+  # Headers
+  NAME = "NAME"
+  EXTRAVERSION = "EXTRAVERSION"
+  AGREEABLENESS = "AGREEABLENESS"
+  CONSCIENTIOUSNESS = "CONSCIENTIOUSNESS"
+  NEUROTICISM = "NEUROTICISM"
+  OPENNESS_TO_EXPERIENCE = "OPENNESS TO EXPERIENCE"
+
+  # Labels
+  OVERALL_SCORE_LABEL = "Overall Score"
+  FACETS_LABEL = "Facets"
+
+  SECTIONS = [
+    EXTRAVERSION,
+    AGREEABLENESS,
+    CONSCIENTIOUSNESS,
+    NEUROTICISM,
+    OPENNESS_TO_EXPERIENCE
+  ]
 
   def initialize(text:)
     @text = text
+    @report_hash = {}
   end
 
   def to_h
+    parse_name
+
+    SECTIONS.each do |section_name|
+      parse_section(section_name: section_name)
+    end
+
+    @report_hash
+  end
+
+  private
+
+  def parse_name
+    @report_hash[NAME] = current_line
+    @text = @text.drop(2) # Name and newline
+  end
+
+  def parse_section(section_name:)
+    if current_line[/#{section_name}/].nil?
+      @text = @text.drop(1)
+      parse_section(section_name: section_name)
+    else
+      overall_score = parse_overall_score(section_name: section_name)
+      @text = @text.drop(2)
+
+      @report_hash[section_name] = {
+        "Overall Score" => overall_score,
+        "Facets" => parse_facets
+      }
+    end
+  end
+
+  def parse_overall_score(section_name: section_name)
+    # Just get the number from the report
+    # i.e. EXTRAVERSION.........74 => 74
+    current_line.split(/[\.]+/)[1].to_i
+  end
+
+  def parse_facets
+    # For 6 facets, extract the label and number
+    # i.e. Friendliness.........74 => "Friendliness": 74
+
+    facets = {}
+    6.times do
+      parsed_line = current_line.split(/[\.]+/)
+      facets[parsed_line[0]] = parsed_line[1].to_i
+      @text = @text.drop(2)
+    end
+    facets
+  end
+
+  def current_line
+    @text[0].chomp
   end
 end
--- spec/big_five_results_text_serializer_spec.rb
+++ spec/big_five_results_text_serializer_spec.rb
@@ -12,60 +12,60 @@ describe BigFiveResultsTextSerializer do
       it "converts it into a results hash" do
         expect(serializer.to_h).to eq(
           {
-            "NAME": "Jah Seng Lee",
-            "EXTRAVERSION": {
-              "Overall Score": 74,
-              "Facets": {
-                "Friendliness": 74,
-                "Gregariousness": 92,
-                "Assertiveness": 81,
-                "Activity Level": 70,
-                "Excitement-Seeking": 56,
-                "Cheerfulness": 18
+            "NAME" => "Jah Seng Lee",
+            "EXTRAVERSION" => {
+              "Overall Score" => 74,
+              "Facets" => {
+                "Friendliness" => 74,
+                "Gregariousness" => 92,
+                "Assertiveness" => 81,
+                "Activity Level" => 70,
+                "Excitement-Seeking" => 56,
+                "Cheerfulness" => 18
               },
             },
-            "AGREEABLENESS": {
-              "Overall Score": 70,
-              "Facets": {
-                "Trust": 92,
-                "Morality": 50,
-                "Altruism": 57,
-                "Cooperation": 63,
-                "Modesty": 82,
-                "Sympathy": 21
+            "AGREEABLENESS" => {
+              "Overall Score" => 70,
+              "Facets" => {
+                "Trust" => 92,
+                "Morality" => 50,
+                "Altruism" => 57,
+                "Cooperation" => 63,
+                "Modesty" => 82,
+                "Sympathy" => 21
               },
             },
-            "CONSCIENTIOUSNESS": {
-              "Overall Score": 56,
-              "Facets": {
-                "Self-Efficacy": 67,
-                "Orderliness": 57,
-                "Dutifulness": 95,
-                "Achievement-Striving": 56,
-                "Self-Discipline": 47,
-                "Cautiousness": 15
+            "CONSCIENTIOUSNESS" => {
+              "Overall Score" => 56,
+              "Facets" => {
+                "Self-Efficacy" => 67,
+                "Orderliness" => 57,
+                "Dutifulness" => 95,
+                "Achievement-Striving" => 56,
+                "Self-Discipline" => 47,
+                "Cautiousness" => 15
               },
             },
-            "NEUROTICISM": {
-              "Overall Score": 17,
-              "Facets": {
-                "Anxiety": 27,
-                "Anger": 14,
-                "Depression": 79,
-                "Self-consciousness": 1,
-                "Immoderation": 28,
-                "Vulnerability": 19
+            "NEUROTICISM" => {
+              "Overall Score" => 17,
+              "Facets" => {
+                "Anxiety" => 27,
+                "Anger" => 14,
+                "Depression" => 79,
+                "Self-Consciousness" => 1,
+                "Immoderation" => 28,
+                "Vulnerability" => 19
               },
             },
-            "OPENNESS_TO_EXPERIENCE": {
-              "Overall Score": 54,
-              "Facets": {
-                "Imagination": 81,
-                "Artistic Interests": 14,
-                "Emotionality": 6,
-                "Adventurousness": 93,
-                "Intellect": 48,
-                "Liberalism": 71
+            "OPENNESS TO EXPERIENCE" => {
+              "Overall Score" => 54,
+              "Facets" => {
+                "Imagination" => 81,
+                "Artistic Interests" => 14,
+                "Emotionality" => 6,
+                "Adventurousness" => 93,
+                "Intellect" => 48,
+                "Liberalism" => 71
               },
             },
           }
